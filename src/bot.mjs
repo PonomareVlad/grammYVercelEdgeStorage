0bb05811aba2593685c24ca48c6d9e35eec83505
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-06-28 18:42:52 +0500

    Mongo Fetch Client

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,17 +1,25 @@
-import {Bot} from "grammy";
+import {Bot, session} from "grammy";
+import {MongoFetchClient} from "mongo-fetch";
+import {MongoDBAdapter} from "@grammyjs/storage-mongodb";
 
 export const {
-
-    // Telegram bot token from t.me/BotFather
+    DATA_API_URL: url,
+    DATA_API_KEY: apiKey,
     TELEGRAM_BOT_TOKEN: token,
-
-    // Secret token to validate incoming updates
     TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(":").pop()
-
 } = process.env;
 
-// Default grammY bot instance
 export const bot = new Bot(token);
+export const client = new MongoFetchClient("Dev", {url, apiKey});
+export const collection = client.db("grammY").collection("users");
 
-// Sample handler for a simple echo bot
+bot.use(
+    session({
+        initial: () => ({counter: 0}),
+        storage: new MongoDBAdapter({collection}),
+    })
+);
+
+bot.command("stats", ctx => ctx.reply(`Already got ${ctx.session.counter} photos!`));
+bot.on(":photo", ctx => ctx.session.counter++);
 bot.on("message:text", ctx => ctx.reply(ctx.msg.text));
